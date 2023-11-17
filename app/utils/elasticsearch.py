4bfa88a345f32bd25b5cb912e84bec0adaf2aa71
szymciem8 <szymciem8@gmail.com>
2023-11-17 15:58:43 +0100

    Fix no openai api key during import

--- app/utils/elasticsearch.py
+++ app/utils/elasticsearch.py
@@ -4,20 +4,19 @@ from langchain.vectorstores.elasticsearch import ElasticsearchStore
 from langchain.embeddings.openai import OpenAIEmbeddings
 from settings import ELASTICSEARCH_HOST
 
-embeddings = OpenAIEmbeddings()
 
 @st.cache_resource
 def get_elasticserach_store(index_name):
     es = ElasticsearchStore(
         es_url=ELASTICSEARCH_HOST,
         index_name=index_name,
-        embedding=embeddings,
+        embedding=OpenAIEmbeddings(),
     )
     return es
 
 
 def upload_docs_to_es(index_name, texts):
     es = ElasticsearchStore.from_texts(
-        texts, embeddings, es_url=ELASTICSEARCH_HOST, index_name=index_name
+        texts, OpenAIEmbeddings(), es_url=ELASTICSEARCH_HOST, index_name=index_name
     )
     return es
